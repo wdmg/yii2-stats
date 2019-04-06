1019ae6f23e81f7648c02db46aa7c0ad2323cd0a
alex-wdmg <wdmg.com.ua@gmail.com>
2019-04-06 16:47:29 +0300

    Added copyrights

--- Bootstrap.php
+++ Bootstrap.php
@@ -2,6 +2,12 @@
 
 namespace wdmg\stats;
 
+/**
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ */
+
 use yii\base\BootstrapInterface;
 use Yii;
 use wdmg\stats\behaviors\ViewBehavior;
--- Module.php
+++ Module.php
@@ -2,6 +2,18 @@
 
 namespace wdmg\stats;
 
+/**
+ * Yii2 Statistics
+ *
+ * @category        Module
+ * @version         1.0.6
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @link            https://github.com/wdmg/yii2-stats
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ *
+ */
+
 use Yii;
 
 /**
