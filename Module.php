972b6e72756a84bdd2332b5f68b729a8bee0884c
alex-wdmg <wdmg.com.ua@gmail.com>
2019-05-29 15:37:11 +0300

    Updated dependencies

--- Module.php
+++ Module.php
@@ -6,7 +6,7 @@ namespace wdmg\stats;
  * Yii2 Statistics
  *
  * @category        Module
- * @version         1.1.0
+ * @version         1.1.1
  * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
  * @link            https://github.com/wdmg/yii2-stats
  * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
@@ -54,7 +54,7 @@ class Module extends \yii\base\Module
     /**
      * @var string the module version
      */
-    private $version = "1.1.0";
+    private $version = "1.1.1";
 
     /**
      * @var integer, priority of initialization
