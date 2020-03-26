eba083070dabf3f5cc7cb94bc4a508894ba3c370
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-03-26 03:00:13 +0200

    Not used stats mode

--- views/visitors/index.php
+++ views/visitors/index.php
@@ -144,7 +144,7 @@ JS
                     'class' => 'btn ' . $buttonClass['year']
                 ]
             ]),
-            Button::widget([
+            /*Button::widget([
                 'label' => Yii::t('app/modules/stats', 'Custom date'),
                 'options' => [
                     'name' => 'period',
@@ -152,7 +152,7 @@ JS
                     'value' => 'custom',
                     'class' => 'btn ' . $buttonClass['custom']
                 ]
-            ]),
+            ]),*/
         ]
     ]); ?>
 
